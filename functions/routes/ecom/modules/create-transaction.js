2149c3d239c2c71f69c703fbbee03b71a86388b4
matheusgnreis <matheus@e-com.club>
2024-03-27 22:41:38 -0300

    chore: test final create transaction

--- functions/routes/ecom/modules/create-transaction.js
+++ functions/routes/ecom/modules/create-transaction.js
@@ -1,49 +1,185 @@
-exports.post = ({ appSdk, admin }, req, res) => {
-  /**
-   * Requests coming from Modules API have two object properties on body: `params` and `application`.
-   * `application` is a copy of your app installed by the merchant,
-   * including the properties `data` and `hidden_data` with admin settings configured values.
-   * JSON Schema reference for the Create Transaction module objects:
-   * `params`: https://apx-mods.e-com.plus/api/v1/create_transaction/schema.json?store_id=100
-   * `response`: https://apx-mods.e-com.plus/api/v1/create_transaction/response_schema.json?store_id=100
-   *
-   * Examples in published apps:
-   * https://github.com/ecomplus/app-pagarme/blob/master/functions/routes/ecom/modules/create-transaction.js
-   * https://github.com/ecomplus/app-custom-payment/blob/master/functions/routes/ecom/modules/create-transaction.js
-   */
+const axios = require('axios')
+const addInstallments = require('../../../lib/payments/add-installments')
+const parseStatus = require('../../../lib/payments/parse-status')
+const getCustomer = require('../../../lib/appmax/customer')
+const createOrder = require('../../../lib/appmax/order')
 
+exports.post = async ({ appSdk }, req, res) => {
+  // https://apx-mods.e-com.plus/api/v1/create_transaction/schema.json?store_id=100
+  let baseUri = 'https://homolog.sandboxappmax.com.br/api/v3/payment/credit-card'
   const { params, application } = req.body
   const { storeId } = req
-  // merge all app options configured by merchant
-  const appData = Object.assign({}, application.data, application.hidden_data)
-  // setup required `transaction` response object
-  const transaction = {}
+  const config = Object.assign({}, application.data, application.hidden_data)
+  const { token } = config
+  const orderId = params.order_id
+  const { amount, buyer, to, items, browser_ip } = params
+  console.log('> Transaction #', storeId, orderId)
+  const quantityItems = items.reduce((previous, currentValue) => previous + currentValue.quantity, 0)
+  const customerId = await getCustomer(buyer, to, items, browser_ip, utm, token)
+  const orderAppmaxId = await createOrder(items, amount, customerId, token)
 
-  // indicates whether the buyer should be redirected to payment link right after checkout
-  let redirectToPayment = false
-
-  /**
-   * Do the stuff here, call external web service or just fill the `transaction` object
-   * according to the `appData` configured options for the chosen payment method.
-   */
-
-  // WIP:
-  switch (params.payment_method.code) {
-    case 'credit_card':
-      // you may need to handle card hash and create transaction on gateway API
-      break
-    case 'banking_billet':
-      // create new "Boleto bancário"
-      break
-    case 'online_debit':
-      redirectToPayment = true
-      break
-    default:
-      break
+  // https://apx-mods.e-com.plus/api/v1/create_transaction/response_schema.json?store_id=100
+  const transaction = {
+    amount: amount.total
   }
 
-  res.send({
-    redirect_to_payment: redirectToPayment,
-    transaction
-  })
+  const appmaxTransaction = {
+    "access-token": token,
+    "cart": {
+      "order_id": orderAppmaxId
+    },
+    "customer": {
+      "customer_id": customerId
+    }
+  }
+
+  if (params.payment_method.code === 'credit_card') {
+    let installmentsNumber = params.installments_number
+    appmaxTransaction.payment = {
+      "CreditCard": {
+        "token": params.credit_card && params.credit_card.hash,
+        "document_number": buyer.doc_number,
+        "installments": installmentsNumber,
+        "soft_descriptor": config.soft_descriptor || (`${params.domain}_APPMAX`).substr(0, 13)
+      }
+    }
+  } else if (params.payment_method.code === 'account_deposit') {
+    baseUri = 'https://homolog.sandboxappmax.com.br/api/v3/payment/pix'
+    const pixConfig = config.account_deposit
+    const dueTime = pixConfig.due_time || 60
+    const date = new Date()
+    date.setTime(date.getTime() + dueTime * 60000)
+    appmaxTransaction.payment = {
+      "pix": {
+        "document_number":buyer.doc_number,
+        "expiration_date": date.toISOString(),
+      }
+    }
+  } else {
+    // banking billet
+    baseUri = 'https://homolog.sandboxappmax.com.br/api/v3/payment/boleto'
+    transaction.banking_billet = {}
+    appmaxTransaction.payment = {
+      "Boleto": {
+        "document_number": buyer.doc_number
+      }
+    }
+
+    const boleto = config.banking_billet
+    if (boleto) {
+      if (boleto.instructions) {
+        appmaxTransaction.boleto_instructions = boleto.instructions
+          .replace(/\r\n/g, '\n')
+          .replace(/\r/g, '\n')
+          .substr(0, 255)
+        transaction.banking_billet.text_lines = [appmaxTransaction.boleto_instructions]
+      }
+    }
+  }
+
+  // https://docs.pagar.me/reference#criar-transacao
+  if (quantityItems > 0) {
+    axios({
+      url: baseUri,
+      method: 'post',
+      data: appmaxTransaction
+    })
+      .then(({ data }) => {
+        console.log('result payment', JSON.stringify(data))
+        if (data.authorized_amount) {
+          transaction.amount = data.authorized_amount / 100
+        } else if (data.amount) {
+          transaction.amount = data.amount / 100
+        }
+        const paymentMethod = data.payment_method === 'pix'
+          ? 'account_deposit'
+          : data.payment_method
+        transaction.intermediator = {
+          payment_method: {
+            code: paymentMethod || params.payment_method.code
+          }
+        }
+        ;[
+          ['id', 'transaction_id'],
+          ['tid', 'transaction_code'],
+          ['reference_key', 'transaction_reference']
+        ].forEach(([dataField, transactionField]) => {
+          if (data[dataField]) {
+            transaction.intermediator[transactionField] = String(data[dataField])
+          }
+        })
+        if (data.customer && data.customer.id) {
+          transaction.intermediator.buyer_id = String(data.customer.id)
+        }
+  
+        if (transaction.banking_billet) {
+          if (data.boleto_barcode) {
+            transaction.banking_billet.code = data.boleto_barcode
+          }
+          if (data.boleto_url) {
+            transaction.payment_link = transaction.banking_billet.link = data.boleto_url
+          }
+          if (data.boleto_expiration_date) {
+            transaction.banking_billet.valid_thru = new Date(data.boleto_expiration_date).toISOString()
+          }
+        } else if (data.card) {
+          transaction.credit_card = {
+            holder_name: data.card.holder_name,
+            last_digits: data.card.last_digits,
+            company: data.card.brand,
+            token: data.card.fingerprint
+          }
+        } else if (paymentMethod === 'account_deposit') {
+          const qrCode = data.pix_qr_code
+          transaction.intermediator.transaction_code = qrCode
+          const qrCodeSrc = `https://gerarqrcodepix.com.br/api/v1?brcode=${qrCode}&tamanho=256`
+          transaction.notes = `<img src="${qrCodeSrc}" style="display:block;margin:0 auto">`
+          if (data.pix_expiration_date) {
+            transaction.account_deposit = {
+              valid_thru: new Date(data.pix_expiration_date).toISOString()
+            }
+          }
+        }
+  
+        transaction.status = {
+          updated_at: data.date_created || data.date_updated || new Date().toISOString(),
+          current: parseStatus(data.status)
+        }
+        res.send({ transaction })
+      })
+  
+      .catch(error => {
+        console.log(error)
+        // try to debug request error
+        const errCode = 'APPMAX_TRANSACTION_ERR'
+        let { message } = error
+        const err = new Error(`${errCode} #${storeId} - ${orderId} => ${message}`)
+        if (error.response) {
+          const { status, data } = error.response
+          if (status !== 401 && status !== 403) {
+            err.payment = JSON.stringify(appmaxTransaction)
+            err.status = status
+            if (typeof data === 'object' && data) {
+              err.response = JSON.stringify(data)
+            } else {
+              err.response = data
+            }
+          } else if (data && Array.isArray(data.errors) && data.errors[0] && data.errors[0].message) {
+            message = data.errors[0].message
+          }
+        }
+        console.error(err)
+        res.status(409)
+        res.send({
+          error: errCode,
+          message
+        })
+      })
+  } else {
+    res.status(400)
+    res.send({
+      error: 400,
+      message: 'Doesnt have quantity to proceed'
+    })
+  }
 }
