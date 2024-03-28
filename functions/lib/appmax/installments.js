1bc290645311727b4b369ca4b2a4314e3cabba43
matheusgnreis <matheus@e-com.club>
2024-03-28 02:03:01 -0300

    chore: create webhook and send installments info

---
+++ functions/lib/appmax/installments.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+
+const createInstallments = async (config, token) => {
+  const body = {
+}
+
+  const { data } = await axios({
+    url: 'https://homolog.sandboxappmax.com.br/api/v3/payment/installments',
+    method: 'post',
+    data: body
+  })
+  console.log('created installments', JSON.stringify(data))
+
+  if (data && data.status === 200) {
+    return data.data && data.data.id
+  }
+  return null
+}
+
+module.exports = createInstallments
