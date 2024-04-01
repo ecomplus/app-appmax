6dd7b338a2fbe2797265a33bb6b48ddb213b1e49
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-04-01 10:50:52 -0300

    chore: update list payments

--- functions/routes/ecom/modules/list-payments.js
+++ functions/routes/ecom/modules/list-payments.js
@@ -9,6 +9,7 @@ exports.post = ({ appSdk }, req, res) => {
   const amount = params.amount || {}
   const initialTotalAmount = amount.total
 
+
   const config = Object.assign({}, application.data, application.hidden_data)
   if (!config.token || !config.public_key) {
     return res.status(409).send({
@@ -121,7 +122,7 @@ exports.post = ({ appSdk }, req, res) => {
         gateway.js_client = {
           script_uri: 'https://assets.pagar.me/pagarme-js/4.8/pagarme.min.js',
           onload_expression: `window._appmaxKey="${config.public_key}";` +
-            fs.readFileSync(path.join(__dirname, '../../../public/onload-expression.js'), 'utf8'),
+          fs.readFileSync(path.join(__dirname, '../../../assets/dist/onload-expression.min.js'), 'utf8'),
           cc_hash: {
             function: '_appmaxHash',
             is_promise: true
