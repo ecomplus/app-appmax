8988968b0fa4de4073ab929343bdbf83e2105013
matheusgnreis <matheus@e-com.club>
2024-04-11 01:53:53 -0300

    feat(webhook): set webhook into payment

--- functions/lib/payments/parse-status.js
+++ functions/lib/payments/parse-status.js
@@ -3,19 +3,15 @@ module.exports = appmaxStatus => {
     case 'processing':
     case 'analyzing':
       return 'under_analysis'
-    case 'authorized':
-    case 'paid':
-    case 'refunded':
-      return appmaxStatus
     case 'waiting_payment':
       return 'pending'
     case 'pending_refund':
       return 'in_dispute'
     case 'refused':
       return 'unauthorized'
-    case 'chargedback':
-      return 'refunded'
-    case 'pending_review':
+    case 'aprovado':
+      return 'paid'
+    case 'autorizado':
       return 'authorized'
   }
   return 'pending'
--- functions/routes/appmax/webhook.js
+++ functions/routes/appmax/webhook.js
@@ -1,80 +1,59 @@
 const getAppData = require('../../lib/store-api/get-app-data')
+const parseStatus = require('../../lib/payments/parse-status')
 const axios = require('axios')
+
+const findOrderByTransactionId = (appSdk, storeId, auth, transactionId) => {
+  return new Promise((resolve, reject) => {
+    appSdk.apiRequest(storeId, `orders.json?transactions.intermediator.transaction_id=${transactionId}`, 'GET', null, auth)
+      .then(({ response }) => {
+        resolve(response.data)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
 
 exports.post = ({ appSdk }, req, res) => {
   // https://docs.pagar.me/docs/gerenciando-postbacks
-  const storeId = req.query.storeId
+  const { storeId, token } = req.query
   console.log('> Postback #', storeId, JSON.stringify(req.body))
-  let appmaxTransaction = req.body && req.body.transaction
-  if (appmaxTransaction && appmaxTransaction.metadata && false) {
-    let storeId = parseInt(appmaxTransaction.metadata.store_id, 10)
-    let orderId = appmaxTransaction.metadata.order_id
-
-    if (storeId > 100 && /^[a-f0-9]{24}$/.test(orderId)) {
-      console.log('> Postback #', storeId, orderId, (req.body.current_status || appmaxTransaction.status))
+  const appmaxTransaction = req.body
+  if (appmaxTransaction && appmaxTransaction.data && appmaxTransaction.event) {
+    if (storeId > 100) {
       // read configured E-Com Plus app data
-      return getAppData({ appSdk, storeId })
-        .then(config => {
-          // validate Pagar.me postback
-          // https://github.com/pagarme/pagarme-js/issues/170#issuecomment-503729557
-          const apiKey = config.pagarme_api_key
-          const verifyBody = qs.stringify(req.body)
-          const signature = req.headers['x-hub-signature'].replace('sha1=', '')
-
-          if (!pagarme.postback.verifySignature(apiKey, verifyBody, signature) && verifyBody && signature) {
-            axios({
-              url: `https://api.pagar.me/1/transactions/${appmaxTransaction.id}`,
-              method: 'get',
-              data: {
-                api_key: apiKey
+      return appSdk.getAuth(storeId)
+        .then(async (auth) => {
+          // validate Appmax postback
+          try {
+            const appData = await getAppData({ appSdk, storeId, auth })
+            if (token === appData.token) {
+              const { id, status } = appmaxTransaction.data
+              const orderRequest = await findOrderByTransactionId(appSdk, storeId, auth, id)
+              if (orderRequest && Array.isArray(orderRequest.result) && orderRequest.result.length) {
+                const order = orderRequest.result[0]
+                const transaction = order.transactions.find(({ intermediator }) => {
+                  return intermediator && intermediator.transaction_id === id
+                })
+                await appSdk.apiRequest(
+                  storeId,
+                  `orders/${order._id}/payments_history.json`,
+                  'POST',
+                  {
+                    date_time: new Date().toISOString(),
+                    status: parseStatus(status),
+                    transaction_id: transaction._id,
+                    flags: ['APPMAX, MGNR']
+                  },
+                  auth
+                )
+                res.status(200)
               }
-            }).then(({data}) => {
-              appmaxTransaction = {}
-              storeId = 1000
-              orderId = 0
-              appmaxTransaction = data
-              storeId = parseInt(appmaxTransaction.metadata.store_id, 10)
-              orderId = appmaxTransaction.metadata.order_id
-              console.log('Get order #', storeId, orderId, appmaxTransaction.status)
-            })
+            }
+          } catch (error) {
+            console.log('erro no update', error)
+            res.status(400).send(error)
           }
-
-          // get E-Com Plus order
-          const resource = `orders/${orderId}.json`
-          return appSdk
-            .apiRequest(storeId, resource)
-            .then(({ response }) => ({ order: response.data, config }))
-        })
-
-        .then(({ order }) => {
-          // add new transaction status to payment history
-          const transaction = order.transactions.find(({ intermediator }) => {
-            return intermediator && intermediator.transaction_id === String(appmaxTransaction.id)
-          })
-          const resource = `orders/${order._id}/payments_history.json`
-          const method = 'POST'
-          const body = {
-            date_time: new Date().toISOString(),
-            status: parseStatus(req.body.current_status || appmaxTransaction.status),
-            notification_code: req.body.fingerprint,
-            flags: ['pagarme']
-          }
-          if (transaction) {
-            body.transaction_id = transaction._id
-          }
-          if (req.body.old_status) {
-            body.flags.push(`old:${req.body.old_status}`.substr(0, 20))
-          }
-          return appSdk.apiRequest(storeId, resource, method, body)
-        })
-
-        .then(() => {
-          res.sendStatus(200)
-        })
-        .catch(err => {
-          err.metadata = appmaxTransaction.metadata
-          console.error(err)
-          res.sendStatus(500)
         })
     }
   }
