31f0e77b8bff2726d1affbb13a1382518d755703
matheusgnreis <matheus@e-com.club>
2024-04-11 22:32:29 -0300

    chore: test webhook

--- functions/routes/appmax/webhook.js
+++ functions/routes/appmax/webhook.js
@@ -19,64 +19,65 @@ const validateStatus = function (status) {
 }
 
 exports.post = ({ appSdk }, req, res) => {
+  console.log('>>Webhook Pagaleve: ')
+  const { body, query } = req
   // https://docs.pagar.me/docs/gerenciando-postbacks
-  const { storeId, token } = req.query
-  console.log('> Postback #', storeId, JSON.stringify(req.body))
+  const { storeId } = query
+  console.log('> Postback #', storeId, JSON.stringify(body))
   const appmaxTransaction = req.body
   if (appmaxTransaction && appmaxTransaction.data && appmaxTransaction.event) {
     if (storeId > 100) {
-      // read configured E-Com Plus app data
+      console.log('entrei com store id', storeId, typeof storeId)
       return appSdk.getAuth(storeId)
         .then(async (auth) => {
-          // validate Appmax postback
           try {
-            console.log('feito login')
-            const appData = await getAppData({ appSdk, storeId, auth })
-            const { id } = appmaxTransaction.data
-            return axios.get(`https://admin.appmax.com.br/api/v3/order/${id}`, {
-                'access-token': appData.token
-              }, {
-                maxRedirects: 0,
-                validateStatus
-              }).then(async ({ data }) => {
-                console.log('order', JSON.stringify(data))
-                const status = data && data.data && data.data.status
-                console.log('status', status)
-                const orderRequest = await findOrderByTransactionId(appSdk, storeId, auth, id)
-                if (orderRequest && Array.isArray(orderRequest.result) && orderRequest.result.length) {
-                  const order = orderRequest.result[0]
-                  const transaction = order.transactions.find(({ intermediator }) => {
-                    return intermediator && intermediator.transaction_id === id
-                  })
-                  if (transaction && transaction._id) {
-                    const eventTreatement = appmaxTransaction.event === 'OrderChargeBackInTreatment' 
-                      ? 'in_dispute'
-                      : appmaxTransaction.event === 'OrderBilletOverdue'
-                        ? 'voided'
-                        : appmaxTransaction.event === 'OrderPixExpired'
+              console.log('feito login')
+              const appData = await getAppData({ appSdk, storeId, auth })
+              const { id } = appmaxTransaction.data
+              return axios.get(`https://admin.appmax.com.br/api/v3/order/${id}`, {
+                  'access-token': appData.token
+                }, {
+                  maxRedirects: 0,
+                  validateStatus
+                }).then(async ({ data }) => {
+                  console.log('order', JSON.stringify(data))
+                  const status = data && data.data && data.data.status
+                  console.log('status', status)
+                  const orderRequest = await findOrderByTransactionId(appSdk, storeId, auth, id)
+                  if (orderRequest && Array.isArray(orderRequest.result) && orderRequest.result.length) {
+                    const order = orderRequest.result[0]
+                    const transaction = order.transactions.find(({ intermediator }) => {
+                      return intermediator && intermediator.transaction_id === id
+                    })
+                    if (transaction && transaction._id) {
+                      const eventTreatement = appmaxTransaction.event === 'OrderChargeBackInTreatment' 
+                        ? 'in_dispute'
+                        : appmaxTransaction.event === 'OrderBilletOverdue'
                           ? 'voided'
-                          : undefined
+                          : appmaxTransaction.event === 'OrderPixExpired'
+                            ? 'voided'
+                            : undefined
 
 
-                    await appSdk.apiRequest(
-                      storeId,
-                      `orders/${order._id}/payments_history.json`,
-                      'POST',
-                      {
-                        date_time: new Date().toISOString(),
-                        status: eventTreatement || parseStatus(status),
-                        transaction_id: transaction._id,
-                        flags: ['APPMAX, MGNR']
-                      },
-                      auth
-                    )
-                    res.status(200)
+                      await appSdk.apiRequest(
+                        storeId,
+                        `orders/${order._id}/payments_history.json`,
+                        'POST',
+                        {
+                          date_time: new Date().toISOString(),
+                          status: eventTreatement || parseStatus(status),
+                          transaction_id: transaction._id,
+                          flags: ['APPMAX, MGNR']
+                        },
+                        auth
+                      )
+                      res.status(200)
+                    }
                   }
-                }
-              })
+                })
           } catch (error) {
-            console.log('erro no update', error)
-            res.status(400).send(error)
+              console.log('erro no update', error)
+              res.status(400).send(error)
           }
         }).catch(err => {
           console.log('not logged', err)
