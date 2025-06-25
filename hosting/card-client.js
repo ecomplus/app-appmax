6048d4be752ee5b5dd88a3d67416e0c7dd082a0c
Leonardo Matos <leomp120894@gmail.com>
2025-06-24 21:56:51 -0300

    fix: card client script using native fetch

--- hosting/card-client.js
+++ hosting/card-client.js
@@ -1,10 +1,12 @@
 ;(function () {
   window._appmaxHash = function (cardClient) {
     return new Promise(function (resolve, reject) {
-      window.axios({
-        method: 'post',
-        url: 'https://admin.appmax.com.br/api/v3/security/visitor',
-        data: {
+      fetch('https://admin.appmax.com.br/api/v3/security/visitor', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
           public_key: window._appmaxKey,
           card: {
             number: cardClient.number,
@@ -13,12 +15,18 @@
             year: cardClient.year,
             cvv: cardClient.cvc
           }
-        }
-      })
-        .then(function ({ data }) {
-          resolve(data.data.token)
         })
-        .catch(reject)
-    })
-  }
-}())
+      })
+        .then(function (response) {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(function (data) {
+          resolve(data.data.token);
+        })
+        .catch(reject);
+    });
+  };
+}());
